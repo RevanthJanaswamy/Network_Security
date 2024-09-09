3154e353bb93fa68f9c251f816b2723474e0b8e4
Janaswamy, Revanth <Revanth.Janaswamy@ncratleos.com>
2024-09-09 20:31:35 +0530

    get_data.py updated

--- get_data.py
+++ get_data.py
@@ -28,22 +28,41 @@ class NetworkDataExtract:
         except Exception as e:
             raise NetworkSecurityException(e, sys)
         
-    def csv_to_json_converter(self):
+    def csv_to_json_converter(self, file_path):
 
         try:
-            pass
+            data=pd.read_csv(file_path)
+            data.reset_index(drop= True, inplace= True)
+            records=list(json.loads(data.T.to_json()).values())
+            return records
+
         except Exception as e:
             raise NetworkSecurityException(e, sys)
 
-    def pushing_data_to_mongodb(self):
+    def pushing_data_to_mongodb(self,records,Database,Collection):
 
         try:
-            pass
+            self.Database=Database
+            self.records=records
+            self.Collection=Collection
+            self.mongo_client = pymongo.MongoClient(MONGO_DB_URL)
+            self.Database = self.mongo_client[self.Database]
+            self.Collection = self.Database[self.Collection]
+            self.Collection.insert_many(self.records)
+            return len(self.records)
+        
+
         except Exception as e:
             raise NetworkSecurityException(e, sys)
         
 if __name__== '__main__':
-    pass
+    file_path = "./Network_Data/NetworkData.csv"
+    Database="KNAcademy"
+    Collection="NetworkData"
+    networkobj= NetworkDataExtract()
+    records = networkobj.csv_to_json_converter(file_path)
+    noofrecords=networkobj.pushing_data_to_mongodb(records,Database,Collection)
+    print(noofrecords)
 
         
 
