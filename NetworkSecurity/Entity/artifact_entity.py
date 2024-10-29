4bbfc8d94de0483df1eccb1f68d88cd3b8662201
Revanth Janaswamy <revanthjanaswamy@gmail.com>
2024-10-29 07:09:53 +0530

    data validation and config entity added

--- NetworkSecurity/Entity/artifact_entity.py
+++ NetworkSecurity/Entity/artifact_entity.py
@@ -9,8 +9,13 @@ class DataIngestionArtifact:
 
 @dataclass
 class DataValidationArtifact:
-      def __init__(self): 
-            pass
+      validation_status: bool
+      valid_train_file_path: str
+      valid_test_file_path: str
+      invalid_train_file_path: str
+      invalid_test_file_path: str
+      drift_report_file_path: str
+
       
 @dataclass
 class DataTransformationArtifact:
